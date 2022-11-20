d779a261ab5ad3770dac1a55f6249af0e8d5c039
Kamil Chojnowski <contact@diath.net>
2022-11-20 05:41:56 +0100

    Add preferred size to Label

--- src/widgets/label.lua
+++ src/widgets/label.lua
@@ -1,6 +1,8 @@
 local Label = ikkuna.class('Label', ikkuna.Widget)
 
 function Label:initialize(args)
+	self.preferredSize = {width = 100, height = 20}
+
 	ikkuna.Widget.initialize(self, args)
 	self.type = ikkuna.WidgetType.Label
 
